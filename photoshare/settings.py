433e72ee734ecda2993888bffbd6345d5291f521
drozdzu88 <78482204+drozdzu88@users.noreply.github.com>
2021-04-17 09:10:34 +0200

    Update settings.py

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('drozdzu88@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('-Re8#12z')
+EMAIL_HOST_USER = os.environ.get('xxxxxxxxxx')
+EMAIL_HOST_PASSWORD = os.environ.get('xxxxxxx')
